ba97fd0b8d9f1fc88ca81e3a6f988ee159ed0329
rkdnjs06 <nawon7526@gmail.com>
2025-11-03 11:15:54 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-# app.py
+import streamlit as st
+st.title('나의 첫 웹 서비스 만들기!')
+st.write('안녕하세요, 만나서 반갑습니다!')
+name=st.text_input('이름을 입력해주세요!')
+if st.button('인사말 생성'):
+  st.write(name+'님! 반갑습니다')
+  st.balloons()
+    # app.py
 import streamlit as st
 
 st.set_page_config(page_title="MBTI → 진로 추천", page_icon="🎯", layout="centered")
